0bc17644587c83ce8102cae922be0d0ae72ec19a
Szabolcs888 <szigli.szabolcs@gmail.com>
2024-11-15 21:48:36 +0100

    Deleting the test folder from .gitignore

---
+++ src/test/java/inventory/util/IdUtilsTest.java
@@ -0,0 +1,17 @@
+package inventory.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IdUtilsTest {
+
+    @Test
+    void testGenerateId() {
+        int id = IdUtils.generateId();
+        System.out.println(id);
+
+        boolean isTheNumberCorrect = id >= 1000000 && id <= 9999999;
+        assertTrue(isTheNumberCorrect, "ID should be between 1000000 and 9999999, but it is not. The number is " + id);
+    }
+}
